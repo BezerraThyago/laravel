75f54f5e30685e347a5cd06e7301340710224d2e
Thyago Bezerra <bezerrathyago@gmail.com>
2020-01-31 00:29:27 -0300

    Migrate

--- teste/database/migrations/2020_01_31_020637_create_product.php
+++ teste/database/migrations/2020_01_31_020637_create_product.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProducts extends Migration
+class CreateProduct extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,10 @@ class CreateProducts extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('stock');
+            $table->double('price');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
